faaff161743964c92f46ed753bfdc63126dd7752
AshishNith <ranjan.ashish9992@gmail.com>
2025-06-14 00:16:40 +0530

    model update

--- BackEnd/routes/chatRoutes.js
+++ BackEnd/routes/chatRoutes.js
@@ -61,7 +61,8 @@ router.post('/', async (req, res) => {
     const response = await axios.post(
       'https://openrouter.ai/api/v1/chat/completions',
       {
-        model: 'deepseek/deepseek-r1-0528:free',
+        // model: 'deepseek/deepseek-r1-0528:free',
+        model: 'meta-llama/llama-3.3-8b-instruct:free',
         messages: [
           { role: 'system', content: systemPrompt },
           { 
