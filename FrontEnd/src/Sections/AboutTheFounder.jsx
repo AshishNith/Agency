6fc13d939643347fd47b1d617ef9be29d3d3cb62
AshishNith <ranjan.ashish9992@gmail.com>
2025-06-30 20:07:38 +0530

    about the auther section

--- FrontEnd/src/Sections/AboutTheFounder.jsx
+++ FrontEnd/src/Sections/AboutTheFounder.jsx
@@ -9,7 +9,7 @@ const founders = [
     name: "Ashish Ranjan",
     role: "Founder and CEO",
     image: "https://media.licdn.com/dms/image/v2/D5603AQF-pv9gNqRAHA/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1727822390724?e=1754524800&v=beta&t=7GH9gr6mDHWE5o-GkRF7okemDuBcxfM6JCGijSVEHlw",
-    bio: "Creative developer focused on crafting beautiful user experiences. Expert in animation and interactive web design.",
+    bio: "Passionate engineer and founder of GoRan, blending AI, automation, and web tech to solve real-world problems.",
     socialLinks: {
       instagram: "https://instagram.com/ig_ashish.ranjan",
       linkedin: "https://www.linkedin.com/in/fictech/",
