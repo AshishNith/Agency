ef140221d3f4f189ef3e22b75197eec93a60db6e
AshishNith <ranjan.ashish9992@gmail.com>
2025-07-14 21:22:53 +0530

    Update

--- FrontEnd/src/Components/Chatbot.jsx
+++ FrontEnd/src/Components/Chatbot.jsx
@@ -38,7 +38,8 @@ const Chatbot = () => {
       setMessages(prev => [...prev, { from: 'bot', isTyping: true }]);
 
       // const reply = await fetch('https://agency-ikgd.vercel.app/api/chat', {
-      const reply = await fetch('http://localhost:5000/chat', {
+      // const reply = await fetch('http://localhost:5000/chat', {
+      const reply = await fetch('https://goran-bot.onrender.com/chat', {
 
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
