7a8b206b1fe9e15f8c18f4da51503d15e97d5365
AshishNith <ranjan.ashish9992@gmail.com>
2025-11-03 00:15:14 +0530

    update

--- FrontEnd/src/admin/Layout.jsx
+++ FrontEnd/src/admin/Layout.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link, Outlet, useLocation } from 'react-router-dom';
-import LOGo from "../../public/Assets/LOGO.png"
+import LOGo from "/Assets/LOGO.png"
 
 
 const AdminLayout = () => {
